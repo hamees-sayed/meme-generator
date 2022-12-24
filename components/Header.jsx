e9b2407d1222fa62b6fad8cf1609b84a37fabb26
Hamees Sayed <hameessayed71@gmail.com>
2022-12-24 20:58:32 +0530

    header icon

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,9 @@
+import headerIcon from "../src/assets/troll-face.png";
+
 export default function Header() {
    return (
       <header className="header">
-         <img src="../src/assets/troll-face.png" className="header--image" />
+         <img src={headerIcon} className="header--image" />
          <h2 className="header--title">Meme Generator</h2>
       </header>
    );
